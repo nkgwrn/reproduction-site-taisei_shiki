79e87c097f4d842e026e993444f62859bc7f3d54
Rino Nakagawara <nakagawara@reclick.co.jp>
2020-06-09 13:37:49 +0900

    スマホナビの実装

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,9 @@
 import { test } from "./component/test"; //拡張子は省略可能
 import { test2 } from "./component/test2"; //拡張子は省略可能
+import { globalNav } from "./component/nav";
 import mvSwiper from "./component/swiper.js";
 
 test();
 test2();
+globalNav();
 mvSwiper();
---
+++ src/js/component/nav.js
@@ -0,0 +1,13 @@
+function globalNav() {
+  console.log("test.");
+
+  const toggle = document.getElementById("js-toggle");
+  const globalNav = document.getElementById("js-globalNav");
+
+  toggle.onclick = function () {
+    toggle.classList.toggle("is-active");
+    globalNav.classList.toggle("is-open");
+  };
+}
+
+export { globalNav };
