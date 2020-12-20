7270cd681690e1b720e1f47a969006e71abeb932
Rino Nakagawara <nakagawara@reclick.co.jp>
2020-12-20 18:25:03 +0900

    Updates

---
+++ js/index.js
@@ -0,0 +1,9 @@
+import { globalNav } from "./component/globalNav";
+import { loading } from "./component/loading";
+import { floatingBanner } from "./component/floatingBanner";
+import mvSwiper from "./component/swiper.js";
+
+globalNav();
+loading();
+mvSwiper();
+floatingBanner();
